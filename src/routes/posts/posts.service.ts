6ff08e90e65b555a226e44f40611af5c1451235f
tientv <tienco201@gmail.com>
2025-04-21 14:03:30 +0700

    feature: validate dot env done

--- src/routes/posts/posts.service.ts
+++ src/routes/posts/posts.service.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Injectable } from '@nestjs/common'
+import envConfig from 'src/shared/config'
 import { PrismaService } from 'src/shared/services/prisma.service'
 
 @Injectable()
@@ -8,6 +9,7 @@ export class PostsService {
   constructor(private readonly prismaService: PrismaService) {}
 
   getPost() {
+    console.log(envConfig.DATABASE_URL)
     return this.prismaService.post.findMany()
   }
 
--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,17 +1,52 @@
+import { plainToInstance } from 'class-transformer'
+import { IsString, validateSync } from 'class-validator'
+import { config } from 'dotenv'
 import fs from 'fs'
 import path from 'path'
 
-// check .env file
+config({
+  path: '.env',
+})
 
+// check .env file
 if (!fs.existsSync(path.resolve('.env'))) {
   console.log('No .env file found')
   process.exit(1)
 }
 
 class ConfigSchema {
+  @IsString()
   DATABASE_URL: string
+
+  @IsString()
   ACCESS_TOKEN_SECRET: string
+
+  @IsString()
   ACCESS_TOKEN_EXPIRES_IN: string
+
+  @IsString()
   REFRESH_TOKEN_SECRET: string
+
+  @IsString()
   REFRESH_TOKEN_EXPIRES_IN: string
 }
+
+const configServer = plainToInstance(ConfigSchema, process.env)
+const errors = validateSync(configServer)
+
+if (errors.length > 0) {
+  console.log('Error in config file')
+  const errorMessages = errors.map((e) => {
+    return {
+      property: e.property,
+      constraints: e.constraints,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      value: e.value,
+    }
+  })
+  throw errorMessages
+}
+
+const envConfig = configServer
+
+export default envConfig
