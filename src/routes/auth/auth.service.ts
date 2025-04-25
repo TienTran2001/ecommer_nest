06e388a98b9299c9239d832b85ff9930f5530e7b
tientv <tienco201@gmail.com>
2025-04-25 11:16:41 +0700

    use type predicate

--- src/routes/auth/auth.service.ts
+++ src/routes/auth/auth.service.ts
@@ -1,9 +1,9 @@
 import { ConflictException, Injectable, UnauthorizedException, UnprocessableEntityException } from '@nestjs/common'
-import { Prisma } from 'generated/prisma'
+import { isNotFoundPrismaError, isUniqueConstraintPrismaError } from 'src/shared/helpers'
 import { HashingService } from 'src/shared/services/hashing.service'
 import { PrismaService } from 'src/shared/services/prisma.service'
-import { LoginBodyDTO, RegisterBodyDTO } from './auth.dto'
 import { TokenService } from 'src/shared/services/token.service'
+import { LoginBodyDTO, RegisterBodyDTO } from './auth.dto'
 
 @Injectable()
 export class AuthService {
@@ -26,11 +26,8 @@ export class AuthService {
 
       return user
     } catch (error) {
-      console.log(error)
-      if (error instanceof Prisma.PrismaClientKnownRequestError) {
-        if (error.code === 'P2002') {
-          throw new ConflictException('Email already exists')
-        }
+      if (isUniqueConstraintPrismaError(error)) {
+        throw new ConflictException('Email already exists')
       }
       throw error
     }
@@ -106,7 +103,7 @@ export class AuthService {
       // step 4: generate new tokens
       return await this.generateTokens({ userId })
     } catch (error) {
-      if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2025') {
+      if (isNotFoundPrismaError(error)) {
         throw new UnauthorizedException('Refresh token has been revoked')
       }
       throw new UnauthorizedException()
